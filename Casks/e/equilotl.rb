eb5afb34c4234d533c14467133aa347af8c0e42d
sdhEmily <sdhEmily@users.noreply.github.com>
2026-04-29 16:04:28 +0000

    equilotl 2.1.7

--- Casks/e/equilotl.rb
+++ Casks/e/equilotl.rb
@@ -1,6 +1,6 @@
 cask "equilotl" do
-  version "2.1.5"
-  sha256 "6b1dbad0c79e34e3f3f9ce18750da1a2f44460a14f5996535080ae76ba73e6a2"
+  version "2.1.7"
+  sha256 "d950c869699670a2c3ae8d0b867541b52270c64641dab2a6900334a567066437"
 
   url "https://github.com/Equicord/Equilotl/releases/download/v#{version}/Equilotl.MacOS.zip"
   name "Equilotl"
