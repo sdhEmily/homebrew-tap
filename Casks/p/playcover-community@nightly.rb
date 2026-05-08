5fca27a073abf9ce1808d434f02b5323f03232b3
Emily <143989797+sdhEmily@users.noreply.github.com>
2026-05-08 10:08:34 +0100

    Add PlayCover Community nightly

---
+++ Casks/p/playcover-community@nightly.rb
@@ -0,0 +1,31 @@
+cask "playcover-community@nightly" do
+  version "1479"
+  sha256 "cadbe71dc0fccbc64dd1860d5580c917951eb0bcafcff96b037a0378d154c092"
+
+  url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop/PlayCover_nightly_#{version}.dmg.zip",
+      verified: "nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/"
+  name "PlayCover"
+  desc "Sideload iOS apps and games"
+  homepage "https://github.com/PlayCover/PlayCover"
+
+  livecheck do
+    url "https://nightly.link/PlayCover/PlayCover/workflows/2.nightly_release/develop?preview"
+    regex(/PlayCover_nightly_(\d+)\.dmg/i)
+  end
+
+  auto_updates true
+  conflicts_with cask: ["playcover-community", "playcover-community@beta"]
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
+end
